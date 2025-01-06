81fb53e490f13b84b2852992aec4cb5b4d9f88fc
bahram demo <bahramunited@gmail.com>
2025-01-06 14:35:29 +1100

    adding the configuration for S3 bucket

--- resource.tf
+++ resource.tf
@@ -1,3 +1,39 @@
 resource "aws_s3_bucket" "bucket1" {
     bucket = "web-bucket-bahram-website"
 }
+resource "aws_s3_bucket_public_access_block" "bucket1" {
+  bucket = aws_s3_bucket.bucket1.id
+
+  block_public_acls       = false
+  block_public_policy     = true
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_object" "index" {
+  bucket = "web-bucket-bahram-website"
+  key    = "index.html"
+  source = "index.html"
+  content_type = "text/html"
+}
+
+resource "aws_s3_object" "error" {
+  bucket = "web-bucket-bahram-website"
+  key    = "error.html"
+  source = "error.html"
+  content_type = "text/html"
+}
+
+
+resource "aws_s3_bucket_website_configuration" "bucket1" {
+  bucket = aws_s3_bucket.bucket1.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+
+}
